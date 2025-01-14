e4a8d9e78b55886d2b6458f7c6ba27a6b12303d3
“” <>
2025-01-14 17:47:40 +0800

    修改头像

--- src/consts.ts
+++ src/consts.ts
@@ -11,7 +11,7 @@ export const DATE_FORMAT = "ddd MMM DD YYYY";
 // User profile information
 export const USER_NAME = "sunwen";
 export const USER_SITE = "https://tosb.cn"; // At the same time, this is also the site retrieved by the i18n configuration.
-export const USER_AVATAR = "/p.gif";
+export const USER_AVATAR = "/a.gif";
 
 // Server and transition settings
 export const SERVER_URL = "https://demo.tosb.cn";
